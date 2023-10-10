d4977285ca256c50372d00422bb49dcfdf91606d
kamerado <kamerado9@gmail.com>
2023-10-10 15:56:14 -0400

    init

--- PyEscapeGame.py
+++ PyEscapeGame.py
@@ -39,11 +39,11 @@ room_desc = {
     'Cell': 'You are in a dark cell.',
     'Cave Hallway': 'You are in a what looks like a Cave Hallway.',
     'Torture Room': 'You are in a small torture room, you see some loose scrap metal.',
-    'Barracks': 'You find an angry looking man eating at a table.',
+    'Barracks': f'You a raider eating at a table.',
     'Medical Room': 'You find a room containing shelfs.',
     'Storage Room': 'You wander down into the Storage Room, there are many storage boxes.',
     'Armory': 'You walk into a Armory, it smells like someone died in here.',
-    'Cave Opening': 'You find the front Cave Opening, but see the raider leader.'}
+    'Cave Opening': f'You find the front Cave Opening, but see the raider leader.'}
 
 # defines room layout.
 rooms = {
@@ -58,8 +58,7 @@ rooms = {
 
 # create item objects and assign them to indavidual rooms.
 class items():
-    _registry = []
-    
+    _registry: list[object] = []
     # set item attributes. Accounting for things like consumable vs non-consumable, location, hp, damage, resistance and name.
     def __init__(self, name, hpAdd, resistAdd, damAdd, useable, location, effects):
         self._registry.append(self) # appends to registry for searching for items in rooms. this makes the item objects iterable.
@@ -111,21 +110,20 @@ class players():
             self.items1 = [] # using 2 items lists for usable and non usable items. will still show in inventory the same.
             self.items2 = []
             self.map = map
-
+        # below are functions used to make gameplay functionality work as expected
         # defines function repsponsible for applying damage to player.
         def takedamage(self, damage, enemy):
+            clear()
             self.hp -= int(damage - ((self.resistance / 100) * damage))
             print(enemy.name, 'attacks for:', damage, "dp.")
-
+            time.sleep(1.75)
         # defines function to calculate how much damage the player deals.
         def calcdam(self):
             dmg = random.randint(self.min_damage, self.max_damage) * self.damage_mult
             return dmg
-        
         def useItem(self):
                 clear()
                 # enumerates through player.items, shows enumerating number next to each item making it easier to select an item.
-                print(self.items2)
                 out = []
                 keys = []
                 for i in self.items2:
@@ -144,7 +142,6 @@ class players():
                         out:
                     print('You have no items to use.')
                     time.sleep(2)
-        
         # defines a function to show current room, hp, inventory.
         def status(self):
             print('Current room is:', self.current_room + '.')
@@ -153,8 +150,7 @@ class players():
                 self.show_items()
             elif self.items1 is None and self.items2 is None:
                 print('You currently have no items.')
-        
-        # defines the function to craft the weapon needed to beat the main villan.
+        # defines the function to craft the weapon needed to beat the main villan. this weapon makes you far stronger than the villan.
         def craft(self):
             if 'iron' in self.items1 and 'steel file' in self.items1\
                     and 'cloth' in self.items1:
@@ -182,7 +178,6 @@ class players():
                 clear()
                 print('There are no items available to craft. Collect more supplies.')
                 time.sleep(2)
-        
         # function to search current room.
         def search_room(self):
             clear()
@@ -190,19 +185,29 @@ class players():
             for i in items._registry:
                 if i.location == self.current_room:
                     room_items = i
-                    print('Items in room:', i.name)
             if room_items == None:
                 clear()
                 print('There are no items in this room.')
+                time.sleep(1.5)
                 return
-                time.sleep(1.5)
-            choice = input('Do you want to take this item? (y/n) ').lower()
-            if choice == 'y':
+            while True:
                 clear()
-                room_items.addItem(self)
-                print(f'You have taken the {room_items.name}.')
-                time.sleep(1.5)
-
+                print('Items in room:', room_items.name)
+                choice = input('Do you want to take this item? (y/n)\n\nchoice: ').lower()
+                if choice == 'y':
+                    clear()
+                    room_items.addItem(self)
+                    print(f'You have taken the {room_items.name}.')
+                    time.sleep(1.5)
+                    break
+                elif choice == 'n':
+                    break
+                else:
+                    clear()
+                    print('Invalid choice.')
+                    time.sleep(2)
+                    choice = None
+                    continue
         # defines function to show current inventory.
         def show_items(self):
             inventory = self.items1
@@ -211,33 +216,88 @@ class players():
                 item2keys += list(self.items2[i].values())
             inventory = self.items1 + item2keys
             print('Current items are:', inventory)
-        
         # defines function that shows the direction of conqurent rooms.
         def possible_movement(self):
             return (f'Possible directions are: {" or ".join(rooms[self.current_room])}')
-        
         # allows player to see instructions whenever needed.
-        def instr():
+        def instr(self):
             clear()
             print(f'\n{room_desc[self.current_room]}')
             print('You need to find the exit, but watch out for you\'re captors!')
             print('Make sure to search the rooms and craft items!')
             time.sleep(5)
+        # function to check if player in an enemy room and initiates fight sequence.
+        def if_fight(self):
+            enemy = None
+            for i in players.enemy._eregistry:
+                if i.current_room == self.current_room:
+                    enemy = i
+            while True:
+                if not enemy == None:
+                    print(f'{room_desc[self.current_room]}')
+                    choice1 = input(f'Do you wish to fight the {enemy.name}? (y/n)\n')
+                    if choice1 == 'y':
+                        loser = fight(self, enemy, self)
+                    elif choice1 == 'n':
+                        print(f'the {enemy.name} notices you and attacks!')
+                        loser = fight(self, enemy, enemy)
+                    else:
+                        clear()
+                        print('Invalid choice.')
+                        time.sleep(1.5)
+                        clear()
+                        continue
+                    if loser == self:
+                        return 'lose'
+                    elif loser == villan:
+                        return 'win'
+                return None
+        # defines function for navigating through conqurent rooms.
+        def goto(self, move):
+            clear()
+            while True:
+                try:
+                    dirs = {'s': 'south', 'e': 'east', 'w': 'west', 'n': 'north'}
+                    move = dirs[move]
+                    break
+                except Exception:
+                    print('Invalid input.')
+                    time.sleep(1.5)
+                    clear()
+                    return None
+            # this if statement makes sure that the movement direction is possible before erasing current position in map.
+            if move not in rooms[self.current_room]:
+                print('You have walked into a wall.')
+                time.sleep(2)
+                return
+            try:
+                self.map = self.map[:map_code[self.current_room]] + ' ' + self.map[map_code[self.current_room]+1:]
+                self.current_room = rooms[self.current_room][move]  # changes current room.
+                self.map = self.map[:map_code[self.current_room]] + '*' + self.map[map_code[self.current_room]+1:]
+                clear()
+                print('You have moved to the', move + '.')
+                time.sleep(2)
+            except Exception:
+                clear()
+                print('You walked into the wall.')
+                time.sleep(2)
 
     # defines enemy character stats.
     class enemy():
+        _eregistry: list[object] = []
         def __init__(self, name, hp, min_damage, max_damage, current_room):
+            self._eregistry.append(self)
             self.name = name
             self.hp = hp
             self.min_damage = min_damage
             self.max_damage = max_damage
             self.current_room = current_room
-
         # defines function for enemy damage delt.
         def takedamage(self, damage):
+            clear()
             self.hp -= damage
             print('You attack', self.name, 'for:', damage, 'dp.')
-
+            time.sleep(1.75)
         # defines function to calculate damage of enemy attacks.
         def calcdam(self):
             dmg = random.randint(self.min_damage, self.max_damage)
@@ -248,58 +308,36 @@ class players():
             return dmg
 
 # creates items and sets atributes and room location.
-item1 = items('healing potion', 50, 0, 0, True, 'Medical Room', '+50 HP.')
+item1 = items('healing potion', 75, 0, 0, True, 'Medical Room', '+75 HP.')
 item2 = items('magic necklace', 0, 15, 0, False, 'Barracks', f'+15% resistance.')
 item3 = items('cloth', 0, 0, 0, False, 'Cave Hallway', None)
 item4 = items('iron', 0, 0, 0, False, 'Torture Room', None)
-item1 = items('resistance potion', 0, 15, 0, True, 'Armory', '+15 Resistance.')
-item1 = items('steel file', 0, 0, 0, False, 'Storage Room', None)
+item5 = items('resistance potion', 0, 15, 0, True, 'Armory', '+15 Resistance.')
+item6 = items('steel file', 0, 0, 0, False, 'Storage Room', None)
 
-# creates enemy objects and sets hp and minimum/maximum attack damage.
-villan = players.enemy('Raider Leader', 250, 35, 65, 'Cave Opening')
-guard = players.enemy('Raider Henchman', 50, 5, 20, 'Barracks')
-
-
-# small introduction into the game, allows you to give yourself a name,
 clear()
-
-print('\nRandom man: Finally you\'re awake! Whats your name?')
+print('\nRandom man: Finally you\'re awake! Whats your name?') # start of intro. must initialize player and enemy objects globally for this to work.
 playername = input('Enter username: ') # allowing user to input a username.
 player = players.hero(playername) # initialize player object.
-clear()
-
-print('\nWell...', player.name + '...\nWe\'ve been captured by the a group')
-print('of raiders who plan to hold us for randsom...')
-action = ''
-while action != 'e':  # using while loop to make sure program won't crash.
-    text = 'Press e to break chains on rocks: '
-    action = input("\x1B[3m" + text + "\x1B[0m")
-clear()
-
-print('\nDon\'t try to escape! They will torture you!')
-time.sleep(2)
+villan = players.enemy('Raider Leader', 250, 35, 65, 'Cave Opening') # creates enemy objects and sets hp and minimum/maximum attack damage.
+guard = players.enemy('Raider Henchman', 50, 5, 20, 'Barracks')
 
 # defines function for player and enemy to fight until either wins.
 def fight(hero, enemy, first_move):
     clear()
     turn = first_move
-    print('You are fighting', enemy.name)
-    if first_move == hero:
-        print('You get to attack first!')
-        time.sleep(2)
-    elif first_move == enemy:
-        print(f'{enemy.name} gets to attack first!')
-        time.sleep(2)
+    print(f'{hero.name} VS {enemy.name}')
+    print(f'{first_move.name} attacks first!')
+    time.sleep(3)
     while hero.hp > 0 and enemy.hp > 0:
         clear()
         if turn == hero:
-            print('Your health:', str(hero.hp) + '.')
-            print('Enemy health:', str(enemy.hp) + '.')
-            print('You can either attack, or use items.')
-            move = input('to attack press "a", to use an item press "u".\n')
+            print(f'{hero.name} health:', str(hero.hp) + '.')
+            print(f'{enemy.name} health:', str(enemy.hp) + '.')
+            print(f'You can either use \'a\' attack, or \'u\' use items.')
+            move = input('choice: \n')
             if move == 'a':
                 enemy.takedamage(hero.calcdam())
-                time.sleep(2)
                 turn = enemy
                 move = ''
             elif move == 'u':
@@ -307,7 +345,6 @@ def fight(hero, enemy, first_move):
                 move = ''
         elif turn == enemy:
             hero.takedamage(enemy.calcdam(), enemy)
-            time.sleep(2)
             turn = hero
     if hero.hp < 0:
         clear()
@@ -323,92 +360,39 @@ def fight(hero, enemy, first_move):
         clear()
         if enemy == villan:
             return villan
-        
-# defines function for navigating through conqurent rooms.
-def goto(room, dir):
-    clear()
-    while True:
-        move = dir
-        if move == 's':
-            move = 'south'
-            break
-        elif move == 'n':
-            move = 'north'
-            break
-        elif move == 'e':
-            move = 'east'
-            break
-        elif move == 'w':
-            move = 'west'
-            break
-        else:
+
+def introdec(function): # decorator for main function. packs intro before main and ending sequence after main.
+    def ending_sequence(result): # sequence that happens when you win or lose.
+        if result == 'win':
             clear()
-            print('Invalid choice.')
-            time.sleep(1.5)
+            print('You have escaped!')
+            print('Winner!')
+        elif result == 'lose':
             clear()
-            return None
-    # this if statement makes sure that the movement direction is possible before erasing current position in map.
-    if move not in rooms[room]:
-        print('You have walked into a wall.')
-        time.sleep(2)
-        return
-    try:
-        player.map = player.map[:map_code[player.current_room]] + ' ' + player.map[map_code[player.current_room]+1:]
-        player.current_room = rooms[room][move]  # changes current room.
-        player.map = player.map[:map_code[player.current_room]] + '*' + player.map[map_code[player.current_room]+1:]
+            print('Collect items and craft a weapon to become stronger.')
+            print('Game Over!')
+    def wrapper():
+        # small introduction into the game, allows you to give yourself a name,
         clear()
-        print('You have moved to the', move + '.')
-        time.sleep(2)
-    except Exception:
+        print('\nWell...', player.name + '...\nWe\'ve been captured by the a group')
+        print('of raiders who plan to hold us for randsom...')
+        action = ''
+        while action != 'e':  # using while loop to make sure program won't crash.
+            text = 'Press e to break chains on rocks: '
+            action = input("\x1B[3m" + text + "\x1B[0m")
         clear()
-        print('You walked into the wall.')
-        print(Exception)
+        print('\nDon\'t try to escape! They will torture you!')
         time.sleep(2)
+        player.instr()
+        result = function() # saving result for ending sequence, if win or lose sequences.
+        ending_sequence(result)
+    return wrapper 
 
-# function to check if player in an enemy room and initiates fight sequence.
-def if_fight():
-    while True:
-        if player.current_room == guard.current_room:
-            print(f'{room_desc[player.current_room]}')
-            choice1 = input(f'Do you wish to fight the {guard.name}? (y/n)\n')
-            if choice1 == 'y':
-                fight(player, guard, player)
-            elif choice1 == 'n':
-                print(f'the {guard.name} notices you and attacks!')
-                fight(player, guard, guard)
-            else:
-                clear()
-                print('Invalid choice.')
-                time.sleep(1.5)
-                clear()
-                continue
-            return None
-
-        elif player.current_room == villan.current_room:
-            print(f'{room_desc[player.current_room]}')
-            choice2 = input(f'Do you wish to fight The {villan.name}? (y/n)\n')
-            if choice2 == 'y':
-                loser = fight(player, villan, player)
-            elif choice2 == 'n':
-                print(f'The {villan.name} attacks!')
-                loser = fight(player, villan, villan)
-            else:
-                clear()
-                print('Invalid choice.')
-                time.sleep('1.5')
-                clear()
-                continue
-            if loser == player:
-                return 'lose'
-            elif loser == villan:
-                return 'win'
-        else:
-            return None
-
+@introdec
 def main():
     clear()
     while player.hp > 0 and villan.current_room == 'Cave Opening':
-        result = if_fight()
+        result = player.if_fight()
         if result != None:  # check for game ending scenareo.
             return result
         clear()
@@ -420,43 +404,19 @@ def main():
         print("'craft' to craft,\n'use' to use items,")
         print("'items' to show items,")
         print("'help' for help.\n")
-        choice = input('Enter move: ').lower()
-        choice = choice.split(' ')
-        try:
-            if choice[0][0] == 'g':
-                goto(player.current_room, choice[1][0])
-            elif choice[0][0] == 's':
-                player.search_room()
-            elif choice[0][0] == 'c':
-                player.craft()
-            elif choice[0][0] == 'u':
-                player.useItem()
-            elif choice[0][0] == 'i':
-                player.show_items()
-            elif choice[0][0] == 'h':
-                player.instr()
-        except Exception:
-            clear()
-            print('Invalid command.')
-            time.sleep(1.5)
-
-# sequence that happens when you win or lose.
-def ending_sequence(result):
-    if result == 'win':
-        clear()
-        print('You have escaped!')
-        print('Winner!')
-        time.sleep(3)
-        clear()
-    elif result == 'lose':
-        clear()
-        print('Collect items and craft a weapon to become stronger.')
-        print('Game Over!')
-
-# function containing main loop.
-if __name__ == '__main__':
-    player.instr()
-    while villan.hp > 0 and player.hp > 0:
-        clear()
-        result = main()
-    ending_sequence(result)
+        choice = []
+        if choice == []:
+            choice = [i for i in input('Enter move: ').lower() if i != ' ']
+            choices = {'g': player.goto, 's': player.search_room, 'c': player.craft, 'u': player.useItem, 'i': player.show_items, 'h':  player.instr}
+            try:
+                if choice[0][0] == 'g':
+                    choices[choice[0][0]](choice[1][0])
+                else:
+                    choices[choice[0][0]]()
+            except Exception:
+                clear()
+                print('Invalid choice.')
+                time.sleep(1)
+                
+if __name__ == '__main__':        
+    main()
